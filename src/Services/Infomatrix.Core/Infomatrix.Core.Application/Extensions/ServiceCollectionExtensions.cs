416ad1793a27b672097536bc82a0b814b367703a
lipkan02 <lipkan02@gmail.com>
2026-03-07 23:35:41 +0200

    Add CQRS pattern and DI registration to Application
    Introduced CQRS interfaces for commands, queries, and handlers under Abstractions.Messaging. Added ServiceCollectionExtensions for automatic handler registration using Scrutor. Included AssemblyReference for assembly scanning. Updated project file to reference Scrutor and DI packages, and added Shared folder.

---
+++ src/Services/Infomatrix.Core/Infomatrix.Core.Application/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Infomatrix.Core.Application.Abstractions.Messaging;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infomatrix.Core.Application.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddCqrsHandlers();
+
+        return services;
+    }
+
+    private static IServiceCollection AddCqrsHandlers(
+        this IServiceCollection services)
+    {
+        return services.Scan(scan => scan
+            .FromAssembliesOf(typeof(AssemblyReference))
+            .AddClasses(classes => classes.AssignableTo(typeof(ICommandHandler<>)), publicOnly: false)
+            .AsImplementedInterfaces()
+            .WithScopedLifetime()
+            .AddClasses(classes => classes.AssignableTo(typeof(ICommandHandler<,>)), publicOnly: false)
+            .AsImplementedInterfaces()
+            .WithScopedLifetime()
+            .AddClasses(classes => classes.AssignableTo(typeof(IQueryHandler<,>)), publicOnly: false)
+            .AsImplementedInterfaces()
+            .WithScopedLifetime());
+    }
+}
